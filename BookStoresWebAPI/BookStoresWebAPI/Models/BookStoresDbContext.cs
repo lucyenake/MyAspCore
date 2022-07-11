7febda11d0804dade8f52fea1b09cb117742b72e
enache.luca <enache.luca@yahoo.com>
2022-07-11 20:55:23 +0300

    Change Db Connection

--- BookStoresWebAPI/BookStoresWebAPI/Models/BookStoresDbContext.cs
+++ BookStoresWebAPI/BookStoresWebAPI/Models/BookStoresDbContext.cs
@@ -29,10 +29,15 @@ namespace BookStoresWebAPI.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            IConfiguration config = new ConfigurationBuilder()
+                          .SetBasePath(Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json", false, false)
+                          .AddEnvironmentVariables()
+                          .Build();
+
             if (!optionsBuilder.IsConfigured)
             {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=BookStoresDB;Trusted_Connection=True;Encrypt=False");
+                _ = optionsBuilder.UseSqlServer(config.GetConnectionString("BookStoresDB"));
             }
         }
 
